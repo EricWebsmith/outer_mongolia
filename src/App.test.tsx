d994579c56688a21aad59910323e0bc620ec3108
Eric Websmith <eric.websmith@gmail.com>
2022-10-10 11:34:46 +0800

    home page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/不承认俄罗斯的任何假投票！！！/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/test/functions.test.tsx
@@ -0,0 +1,19 @@
+describe('tests', ()=>{
+
+    test('test1', ()=>{
+        expect(20+1).toEqual(21);
+    })
+
+});
+
+test('test1', ()=>{
+    expect(20+1).toEqual(21);
+});
+
+test('test2', ()=>{
+    expect(20+1).toEqual(21);
+});
+
+test('test3', ()=>{
+    expect(20+1).toEqual(21);
+});
